5bcd7be3e97078b9fc432423ab478f0a5a95ecc4
tuelsch <tuelsch@gmail.com>
2015-02-09 17:25:10 +0100

    Updated example after SO response

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,51 @@
-var gulp 			= require('gulp');
-var plumber 		= require('gulp-plumber');
-var less 			= require('gulp-less');
-var rename 			= require('gulp-rename');
-var minify			= require('gulp-minify-css');
-var sourcemaps 		= require('gulp-sourcemaps');
-var gutil			= require('gulp-util');
-var autoprefixer 	= require('gulp-autoprefixer');
-var concat			= require('gulp-concat');
-var remember		= require('gulp-remember');
-var cached			= require('gulp-cached');
-var duration		= require('gulp-duration');
-var watch			= require('gulp-watch');
-var changed			= require('gulp-changed');
-var path 			= require('path');
-var source 			= require('vinyl-source-stream');
+var gulp                 = require('gulp');
+var less                 = require('gulp-less');
+var concat               = require('gulp-concat');
+var remember             = require('gulp-remember');
+var cached               = require('gulp-cached');
+var sourcemaps           = require('gulp-sourcemaps');
+var changed              = require('gulp-changed');
+var using                = require('gulp-using');
+var lessPluginCleanCSS   = require('less-plugin-clean-css');
+var lessPluginAutoprefix = require('less-plugin-autoprefix');
 
-// Different sources
-var bootstrapMain = 'bootstrap/bootstrap.less';
-var bootstrapGlob = [
-	'./bootstrap/**/*.less',
-	'!./bootstrap/bootstrap.less',
-	'!./bootstrap/mixins.less'
+var dest = './output';
+var glob = ['./less/**/*.less'];
+var browsers = [
+	'Android >= 2.3',
+	'Chrome >= 20',
+	'Firefox >= 24', // Firefox 24 is the latest ESR
+	'Explorer >= 8',
+	'iOS >= 6',
+	'Opera >= 12',
+	'Safari >= 6'
 ];
-var noImports = './no_imports/**/*.less';
-
-// For switching sources faster
-//var glob = bootstrapMain;
-//var glob = bootstrapGlob;
-var glob = noImports;
+var autoprefix = new lessPluginAutoprefix({browsers: browsers});
+var cleancss = new lessPluginCleanCSS();
 
 // Default task
-gulp.task('default', ['watch']);
+gulp.task('default', ['less', 'watch']);
 
 // Build task
 gulp.task('less', function () {
 	return gulp.src(glob)
 		.pipe(cached('less'))
-		.pipe(remember('less'))
-		.pipe(concat('onefile.less'))
-		.pipe(less())
-		.pipe(minify())
-		.pipe(autoprefixer())
-		.pipe(gulp.dest('output'));
+		.pipe(sourcemaps.init())
+		.pipe(
+			less(
+				{
+					plugins: []
+				}
+			)
+		)
+		.on('error', function (err, some) {
+			console.log(err, some);
+		})
+		.pipe(using())
+		.pipe(remember('css'))
+		.pipe(concat('main.css'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest(dest));
 });
 
 // Watcher task
@@ -50,7 +54,7 @@ gulp.task('watch', function () {
 	watcher.on('change', function (e) {
 		if (e.type === 'deleted') {
 			delete cached.caches.scripts[e.path];
-			remember.forget('less', e.path);
+			remember.forget('css', e.path);
 		}
 	});
 });
